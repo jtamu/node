a9c013627385c3b588b07b36cfebd1f7c2742e5e
tamura-j <tamura-j@icz.co.jp>
2020-12-16 23:42:10 +0900

    リファクタリング フォーマット関数の名前変更

--- src/refactoring/sample1/sample1.js
+++ src/refactoring/sample1/sample1.js
@@ -10,10 +10,10 @@ function statement(invoice, plays) {
     const play = playFor(perf);
 
     volumeCredits += volumeCreditsFor(perf);
-    result += `  ${playFor(perf).name}: ${format(amountFor(perf)/100)} (${perf.audience} seats)\n`;
+    result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
     totalAmount += amountFor(perf);
   }
-  result += `Amount owed is ${format(totalAmount/100)}\n`;
+  result += `Amount owed is ${usd(totalAmount)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
   return result;
 }
@@ -29,11 +29,11 @@ function volumeCreditsFor(performance) {
   return volumeCredits;
 }
 
-function format(number) {
+function usd(number) {
   return new Intl.NumberFormat("en-US", {
     style: "currency", currency: "USD",
     minimumFractionDigits: 2
-  }).format(number);
+  }).format(number/100);
 }
 
 function amountFor(performance) {

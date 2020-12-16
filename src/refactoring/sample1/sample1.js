cc8a04ca840ccccaf8b7f29ebd5e17b2b10e054e
tamura-j <tamura-j@icz.co.jp>
2020-12-16 23:59:16 +0900

    totalAmount変数の削除

--- src/refactoring/sample1/sample1.js
+++ src/refactoring/sample1/sample1.js
@@ -2,17 +2,14 @@ import { invoices } from './invoices.js'
 import { plays } from './plays.js'
 
 function statement(invoice, plays) {
-  let totalAmount = 0;
   let result = `Statement for ${invoice.customer}\n`;
 
   for (let perf of invoice.performances) {
     const play = playFor(perf);
-
     result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
-    totalAmount += amountFor(perf);
   }
 
-  result += `Amount owed is ${usd(totalAmount)}\n`;
+  result += `Amount owed is ${usd(totalAmount(invoice))}\n`;
   result += `You earned ${totalVolumeCredits(invoice)} credits\n`;
   return result;
 }
@@ -27,6 +24,14 @@ function totalVolumeCredits(invoice) {
     volumeCredits += volumeCreditsFor(perf);
   }
   return volumeCredits;
+}
+
+function totalAmount(invoice) {
+  let totalAmount = 0;
+  for (let perf of invoice.performances) {
+    totalAmount += amountFor(perf);
+  }
+  return totalAmount;
 }
 
 function volumeCreditsFor(performance) {

1ca027c23bd35505ab350390193270538df88f34
tamura-j <tamura-j@icz.co.jp>
2020-12-16 23:26:25 +0900

    リファクタリング thisAmount変数のインライン化

--- src/refactoring/sample1/sample1.js
+++ src/refactoring/sample1/sample1.js
@@ -13,12 +13,10 @@ function statement(invoice, plays) {
   for (let perf of invoice.performances) {
     const play = playFor(perf);
 
-    let thisAmount = amountFor(perf);
-
     volumeCredits += Math.max(perf.audience - 30, 0);
     if ("comedy" == playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
-    result += `  ${playFor(perf).name}: ${format(thisAmount/100)} (${perf.audience} seats)\n`;
-    totalAmount += thisAmount;
+    result += `  ${playFor(perf).name}: ${format(amountFor(perf)/100)} (${perf.audience} seats)\n`;
+    totalAmount += amountFor(perf);
   }
   result += `Amount owed is ${format(totalAmount/100)}\n`;
   result += `You earned ${volumeCredits} credits\n`;

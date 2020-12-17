a8c72bca9771fb3ba75de1db5167709c1de86ea9
tamura-j <tamura-j@icz.co.jp>
2020-12-17 22:40:10 +0900

    リファクタリング invoice.customerを中間データ構造に渡す

--- src/refactoring/sample1/sample1.js
+++ src/refactoring/sample1/sample1.js
@@ -3,11 +3,12 @@ import { plays } from './plays.js'
 
 function statement(invoice, plays) {
   const statementData = {};
+  statementData.customer = invoice.customer;
   return renderPlainText(statementData, invoice, plays);
 }
 
 function renderPlainText(data, invoice, plays) {
-  let result = `Statement for ${invoice.customer}\n`;
+  let result = `Statement for ${data.customer}\n`;
 
   for (let perf of invoice.performances) {
     const play = playFor(perf);

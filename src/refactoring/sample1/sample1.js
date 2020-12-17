073391add6fb1c67f88f80ebb93235027bd12eba
tamura-j <tamura-j@icz.co.jp>
2020-12-17 22:35:38 +0900

    リファクタリング renderPlainText関数の作成

--- src/refactoring/sample1/sample1.js
+++ src/refactoring/sample1/sample1.js
@@ -2,6 +2,10 @@ import { invoices } from './invoices.js'
 import { plays } from './plays.js'
 
 function statement(invoice, plays) {
+  return renderPlainText(invoice, plays);
+}
+
+function renderPlainText(invoice, plays) {
   let result = `Statement for ${invoice.customer}\n`;
 
   for (let perf of invoice.performances) {

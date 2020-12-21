263cadc0284e4eff24bd2bf21bb502654b3462d9
tamura-j <tamura-j@icz.co.jp>
2020-12-21 23:13:15 +0900

    リファクタリング html版を実装

--- src/refactoring/sample1/statement.js
+++ src/refactoring/sample1/statement.js
@@ -18,6 +18,23 @@ function renderPlainText(data) {
   return result;
 }
 
+function htmlStatement(invoice) {
+  return renderHtml(createStatementData(invoice));
+}
+
+function renderHtml(data) {
+  let result = `<h1>Statement for ${data.customer}</h1>\n`;
+  result += "<table>\n";
+  result += "  <tr><th>play</th><th>seats</th><th>cost</th></tr>\n";
+  for (let perf of data.performances) {
+    result += `  <tr><td>${perf.play.name}</td><td>${perf.audience}</td><td>${usd(perf.amount)}</td></tr>\n`;
+  }
+  result += "</table>\n";
+  result += `<p>Amount owed is <em>${usd(data.totalAmount)}</em></p>\n`;
+  result += `<p>You earned <em>${data.totalVolumeCredits}</em> credits</p>\n`;
+  return result;
+}
+
 function playFor(performance) {
   return plays[performance.playID];
 }
@@ -40,5 +57,5 @@ function usd(number) {
 }
 
 invoices.forEach((invoice, i) => {
-  console.log(statement(invoice, plays))
+  console.log(htmlStatement(invoice, plays))
 });
